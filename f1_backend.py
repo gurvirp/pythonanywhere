43b53fd7bb9de822f41efd237b80f4d651e29aad
gurvirp <11gpabla@mybest.org.uk>
2024-05-26 17:18:33 +0000

    created hisotric data manager and database manager

--- f1_backend.py
+++ f1_backend.py
@@ -2,6 +2,8 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template, request, jsonify
+from historic_data_manager.historic_data_manager import Historic_Data_Manager
+from database_manager.database_manager import Database_Manager
 
 
 app = Flask(__name__)
@@ -10,3 +12,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+theDatabaseManager = Database_Manager()
+my_historic_data_manager = Historic_Data_Manager(theDatabaseManager)
+champions_data = my_historic_data_manager.obtainF1ChampionsData()
+theDatabaseManager.storeF1ChampionsData(champions_data)
